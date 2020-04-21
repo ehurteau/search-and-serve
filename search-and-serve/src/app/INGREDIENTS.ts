6cb8cb394212bb997078260b21609aa3115f6e96
Ariel Sievers <booboo111998@hotmail.com>
2020-04-21 18:33:04 -0500

    create general structure and functionality for ingredients list

---
+++ search-and-serve/src/app/INGREDIENTS.ts
@@ -0,0 +1,28 @@
+import { Ingredient } from './Ingredient';
+
+export const INGREDIENTS: Ingredient[] = [
+    {
+        id: 0,
+        name: 'banana',
+    },
+    {
+        id: 1,
+        name: 'bread',
+    },
+    {
+        id: 2,
+        name: 'carrot',
+    },
+    {
+        id: 3,
+        name: 'milk',
+    },
+    {
+        id: 4,
+        name: 'water',
+    },
+    {
+        id: 5,
+        name: 'spaghetti pasta',
+    }
+]
--- search-and-serve/src/app/ingredients-list/ingredients-list.component.ts
+++ search-and-serve/src/app/ingredients-list/ingredients-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../ingredient';
+import { INGREDIENTS } from '../INGREDIENTS';
 
 @Component({
   selector: 'app-ingredients-list',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingredients-list.component.css']
 })
 export class IngredientsListComponent implements OnInit {
+  ingredients: Ingredient[]; // list to store ingredients
 
   constructor() { }
 
   ngOnInit() {
+    this.getIngredients();
+  }
+
+  /**
+   * Return the list of ingredients
+   */
+  getIngredients() {
+    this.ingredients = INGREDIENTS; // TODO: implement service in future use
+  }
+  
+  /**
+   * Return an ingredient from the ingredients list
+   * @param id the id of the ingredient
+   */
+  getIngredient(id: number) {
+    this.ingredients.find(ingredient => id == ingredient.id) // TODO: implement service in future use
+  }
+  
+  /**
+   * Remove an ingredient from the ingredients list
+   * @param id the id of the ingredient
+   */
+  removeIngredient(id: number) {
+    this.ingredients.filter( ingredient => id != ingredient.id); // TODO: implement service in future use
   }
 
 }
